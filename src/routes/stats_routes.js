2fd4306de66811967369d4747c035e40e3aaa9c6
Xavier <luis.ochoa02@epn.edu.ec>
2026-07-05 17:55:27 -0500

    Delete src directory

--- src/routes/stats_routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getMyStats } from '../controllers/stats_controller.js'
-import { requireAuth } from '../middleware/requireAuth.js'
-
-const router = Router()
-
-router.get('/me', requireAuth, getMyStats)
-
-export default router
