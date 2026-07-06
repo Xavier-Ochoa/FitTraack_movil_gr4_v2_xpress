95cca90dd8c4870de7027da583ad3984e5a90500
Xavier <luis.ochoa02@epn.edu.ec>
2026-07-05 21:22:29 -0500

    delete_v1

--- src/routes/quotes_routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getRandomQuote } from '../controllers/quotes_controller.js'
-import { requireAuth } from '../middleware/requireAuth.js'
-
-const router = Router()
-
-router.get('/random', requireAuth, getRandomQuote)
-
-export default router
