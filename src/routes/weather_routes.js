e48e0d994dbb3919f137584710c4f6318897ca9a
Xavier <luis.ochoa02@epn.edu.ec>
2026-07-10 11:53:27 -0500

    Delete src directory

--- src/routes/weather_routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { getWeather } from '../controllers/weather_controller.js'
-import { requireAuth } from '../middleware/requireAuth.js'
-
-const router = Router()
-
-router.get('/', requireAuth, getWeather)
-
-export default router
